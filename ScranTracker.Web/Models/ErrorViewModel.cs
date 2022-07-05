8b13a711f9497e1da699e82e581617b44a06b29e
JoshHarrisonDev <joshharrison@hotmail.co.uk>
2022-07-05 19:50:33 +0100

    Add project files.

---
+++ ScranTracker.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ScranTracker.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
